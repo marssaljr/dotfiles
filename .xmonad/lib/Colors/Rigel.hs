20afb0f066fd9f138889181140a3e2c91f2342d5
Marçal Junior <marssaljr@gmail.com>
2022-02-10 01:32:30 -0300

    add xmonad lib

---
+++ .xmonad/lib/Colors/Rigel.hs
@@ -0,0 +1,29 @@
+module Colors.Rigel where
+
+import XMonad
+
+colorScheme = "rigel"
+
+colorBack = "#002635"
+colorFore = "#e6e6dc"
+
+color01 = "#00384d"
+color02 = "#c43061"
+color03 = "#7fc06e"
+color04 = "#f08e48"
+color05 = "#1c8db2"
+color06 = "#c694ff"
+color07 = "#00cccc"
+color08 = "#77929e"
+color09 = "#517f8d"
+color10 = "#ff5a67"
+color11 = "#9cf087"
+color12 = "#ffcc1b"
+color13 = "#7eb2dd"
+color14 = "#fb94ff"
+color15 = "#00ffff"
+color16 = "#b7cff9"
+
+colorTrayer :: String
+colorTrayer = "--tint 0x002b36"
+
